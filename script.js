db6a966d238a889ac83da5898babed3d2eedcc4c
alisonchiu <alisonlchiu@gmail.com>
2018-07-30 19:31:51 -0700

    added additional chart

--- script.js
+++ script.js
@@ -5,30 +5,52 @@ google.charts.load('current', {'packages':['corechart']});
 
 // Set a callback to run when the Google Visualization API is loaded.
 google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawAllocationChart);
 
 // Callback that creates and populates a data table,
 // instantiates the pie chart, passes in the data and
 // draws it.
 function drawChart() {
 
-// Create the data table.
-var data = new google.visualization.DataTable();
-data.addColumn('string', 'Topping');
-data.addColumn('number', 'Amount');
-data.addRows([
-  ['Mushrooms', 3],
-  ['Onions', 1],
-  ['Basil', 1],
-  ['Sun-Dried Tomato', 1],
-  ['Chicken', 2]
-]);
+	// Create the data table.
+	var data = new google.visualization.DataTable();
+	data.addColumn('string', 'Score Range');
+	data.addColumn('number', 'Games');
+	data.addRows([
+	  ['0-100', 2],
+	  ['100-200', 5],
+	  ['200-300', 3]
+	]);
 
-// Set chart options
-var options = {'title':'Amount of toppings on my pizza last night',
-               'width':400,
-               'height':300};
+	// Set chart options
+	var options = {'title':'Bowling Score Incidences',
+	               'width':400,
+	               'height':300};
 
-// Instantiate and draw our chart, passing in some options.
-var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-chart.draw(data, options);
+	// Instantiate and draw our chart, passing in some options.
+	var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+	chart.draw(data, options);
+}
+
+
+function drawAllocationChart() {
+
+	// Create the data table.
+	var data = new google.visualization.DataTable();
+	data.addColumn('string', 'Score Range');
+	data.addColumn('number', 'Games');
+	data.addRows([
+	  ['0-100', 2],
+	  ['100-200', 5],
+	  ['200-300', 3]
+	]);
+
+	// Set chart options
+	var options = {'title':'Bowling Score Incidences',
+	               'width':400,
+	               'height':300};
+
+	// Instantiate and draw our chart, passing in some options.
+	var chart = new google.visualization.PieChart(document.getElementById('allocation'));
+	chart.draw(data, options);
 }
